9b7589ba05185d6d09f93753c2cb191ccf5c3287
Renato Melo <renato.bmelo@outlook.com>
2024-03-02 16:13:30 -0300

    Finalizado

--- src/main/kotlin/Aula2-OO.kt
+++ src/main/kotlin/Aula2-OO.kt
@@ -2,66 +2,84 @@ package br.ifpb.pdm
 
 fun main() {
     val repositorioAnimal = RepositorioAnimal()
-    var opcao = 4
-    while (opcao != 9) {
+    var opcao = 5
+    while (opcao != 11) {
         menu()
         print("Digite a opção: ")
         opcao = readlnOrNull()?.toInt() ?: 4
         when (opcao) {
             1 -> {
-                val cachorro = Cachorro(10)
-                cachorro.nome = "Rex"
+                println("Qual o Nome do Cachorro?")
+                val nomeCachorro = readlnOrNull()?.toString() ?: ""
+                println("Qual a Idade do Cachorro?")
+                val idadeCachorro = readlnOrNull()?.toInt() ?: 0
+                val cachorro = Cachorro(nomeCachorro ,idadeCachorro)
                 repositorioAnimal.adicionar(cachorro)
             }
             2 -> {
-                val gato = Gato(5)
-                gato.nome = "Felix"
+                println("Qual o Nome do Gato?")
+                val nomeGato = readlnOrNull()?.toString() ?: ""
+                println("Qual o Idade do Gato?")
+                val idadeGato = readlnOrNull()?.toInt() ?: 0
+                val gato = Gato(nomeGato, idadeGato)
                 repositorioAnimal.adicionar(gato)
             }
             3 -> {
-                val passaro = Passaro(2)
-                passaro.nome = "Piu"
+                println("Qual o Nome do Pássaro?")
+                val nomePassaro = readlnOrNull()?.toString() ?: ""
+                println("Qual o Idade do Pássaro?")
+                val idadePassaro = readlnOrNull()?.toInt() ?: 0
+                val passaro = Passaro(nomePassaro, idadePassaro)
                 repositorioAnimal.adicionar(passaro)
             }
             4 -> {
-                repositorioAnimal.listar()
+                println("Qual o Nome do Humano?")
+                val nomeHumano = readlnOrNull()?.toString() ?: ""
+                println("Qual o Idade do Humano?")
+                val idadeHumano = readlnOrNull()?.toInt() ?: 0
+                val Humano = Homem(nomeHumano, idadeHumano)
+                repositorioAnimal.adicionar(Humano)
             }
             5 -> {
+                repositorioAnimal.listar()
+            }
+            6 -> {
                 repositorioAnimal.animais.forEach(Animal::emitirSom)
                 repositorioAnimal.animais.forEach { it.emitirSom()}
             }
-            6 -> {
+            7 -> {
                 println("Digite o nome do animal que deseja excluir:")
                 val nome = readlnOrNull()?.toString() ?: "";
                 repositorioAnimal.remover(nome);
             }
-            7 -> {
+            8 -> {
                 println("Digite a cor dos animais que deseja visualizar:")
                 val cor = readlnOrNull()?.toString() ?: "";
                 repositorioAnimal.listarCor(cor).forEach{ println(it.nome) }
             }
-            8 -> {
+            9 -> {
                 println("Digite a idade dos animais que deseja visualizar:")
                 val idade = readlnOrNull()?.toInt() ?: 0;
                 repositorioAnimal.listarIdade(idade).forEach{ println(it.nome) }
+            }
+            10 -> {
+                println("Digite o nome do Animal que deseja visualizar:")
+                val procurarAnimal = readlnOrNull()?.toString() ?: "";
+                val animalEncontrado = repositorioAnimal.buscarPorNome(procurarAnimal)
+                println(animalEncontrado)
             }
         }
 
     }
 }
 
-abstract class Animal(var idade: Int, var cor: Cor) {
-    open var nome: String = ""
-        get() = "$field"
-        set(valor) {
-            field = valor
-        }
+abstract class Animal(var nome: String, var idade: Int, var cor: Cor) {
 
     abstract fun emitirSom()
     abstract fun idadeEmAnosHumanos()
 }
 
-class Cachorro(idade: Int) : Animal(idade, Cor.VERMELHO) {
+class Cachorro(nome: String, idade: Int) : Animal(nome, idade, Cor.VERMELHO) {
     override fun emitirSom() {
         println("Au au")
     }
@@ -70,7 +88,7 @@ class Cachorro(idade: Int) : Animal(idade, Cor.VERMELHO) {
         println(idade*7)
     }
 }
-class Gato(idade: Int) : Animal(idade, Cor.AZUL) {
+class Gato(nome:String, idade: Int) : Animal(nome, idade, Cor.AZUL) {
     override fun emitirSom() {
         println("Miau")
     }
@@ -79,7 +97,7 @@ class Gato(idade: Int) : Animal(idade, Cor.AZUL) {
     }
 }
 
-class Passaro(idade: Int) : Animal(idade, Cor.VERDE) {
+class Passaro(nome:String, idade: Int) : Animal(nome, idade, Cor.VERDE) {
     override fun emitirSom() {
         println("Piu piu")
     }
@@ -88,15 +106,27 @@ class Passaro(idade: Int) : Animal(idade, Cor.VERDE) {
     }
 }
 
+class Homem(nome:String, idade: Int) : Animal(nome, idade, Cor.AMARELO) {
+    override fun emitirSom() {
+        println("Olá")
+    }
+    override fun idadeEmAnosHumanos() {
+        println(idade)
+    }
+}
+
 fun menu() {
     println("1 - Cachorro")
     println("2 - Gato")
     println("3 - Pássaro")
-    println("4 - Listar Animais")
-    println("5 - Emitir som")
-    println("6 - Remover Animal")
-    println("7 - Listar por cor")
-    println("8 - Listar por idade")
+    println("4 - Homens")
+    println("5 - Listar Animais")
+    println("6 - Emitir som")
+    println("7 - Remover Animal")
+    println("8 - Listar por cor")
+    println("9 - Listar por idade")
+    println("10 - Procurar animal pelo Nome")
+    println("11 - Sair")
 }
 
 class RepositorioAnimal {
@@ -106,20 +136,25 @@ class RepositorioAnimal {
         animais.add(animal)
     }
 
+    fun buscarPorNome(nome: String): String? {
+        for (animal in animais) {
+            if (animal.nome == nome) {
+                return "Animal encontrado: ${animal.nome}, ${animal.idade}, ${animal.cor}"
+            }
+        }
+        return "Animal não encontrado"
+    }
+
     fun remover(nome: String) {
         var index: Int = 0;
         for (i in animais.withIndex()) {
-            println("${i.value} ${i.index} $index")
+            //println("${i.value} ${i.index} $index")
             if (i.value.nome == nome) {
                 index = i.index;
             }
         }
         animais.removeAt(index);
-
     }
-
-    fun filtarNome()
-
     fun listarCor(cor: String): MutableList<Animal> {
         var filtrados = mutableListOf<Animal>();
         for (animal in animais) {
@@ -148,5 +183,6 @@ class RepositorioAnimal {
 enum class Cor(val cor: String) {
     VERMELHO("vermelho"),
     VERDE("verde"),
-    AZUL("azul")
+    AZUL("azul"),
+    AMARELO("amarelo")
 }
